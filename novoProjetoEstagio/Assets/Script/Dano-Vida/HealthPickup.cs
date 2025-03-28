10c07d187804f3fb2fe7bbd77492b6ca2f66d3ed
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-03-28 01:46:58 -0100

    commit

--- novoProjetoEstagio/Assets/Script/Dano-Vida/HealthPickup.cs
+++ novoProjetoEstagio/Assets/Script/Dano-Vida/HealthPickup.cs
@@ -2,6 +2,5 @@ using UnityEngine;
 
 public class HealthPickup : MonoBehaviour
 {
-    // Esse script pode ser deixado vazio, pois a lógica de aumento de vida já está no PlayerController.
-    // O objetivo desse script é simplesmente identificar que o objeto é um "HealthPickup" e ser destruído ao ser tocado.
+ 
 }
